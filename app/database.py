9529278cb3579b2e46d89c435dfbb0555dff4c4f
Ammar Hassan <ammar2195575@gmail.com>
2026-05-28 09:42:24 +0500

    Reset database

--- app/database.py
+++ app/database.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./cleancity.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
+
+# Tables drop karke dobara banao
+Base.metadata.drop_all(bind=engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
